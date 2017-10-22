5f11e76d16e8048ebdf6b9a2ebec46443caf5a09
Rabby Hasan <rabbyalone@hotmail.com>
2017-10-23 00:58:22 +0600

    added mail function

---
+++ SendMail.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+
+public partial class SendMail : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string name = String.Format("{0}", Request.Form["c_name"]);
+        string email = String.Format("{0}", Request.Form["c_email"]);
+        string message = String.Format("{0}", Request.Form["c_message"]);
+        
+        if (message != string.Empty)
+        {
+            string fromMailAddress = "xcuse.ffr@gmail.com";
+            string toMailAddress = "rabbyalone@gmail.com";
+
+            string toMailSubject = "Mail From: " + name;
+            string mailBody = "++++**** MESSAGE FROM PERSONAL WEBSITE****++++ <br/>" + "____" + email + "____" + message;
+
+            //Mail Client Configuration 
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress(fromMailAddress);
+            msg.To.Add(toMailAddress);
+            msg.Body = mailBody;
+            msg.IsBodyHtml = true;
+            msg.Subject = toMailSubject;
+
+            //Mail Configuration 
+            #region abc
+            string password = "!Rumirh!";
+            #endregion
+            SmtpClient smt = new SmtpClient("smtp.gmail.com");
+            smt.Port = 587;
+            smt.EnableSsl = true;
+            smt.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smt.UseDefaultCredentials = false;
+            smt.Credentials = new NetworkCredential(fromMailAddress, password);
+            smt.Send(msg);
+            string script = "<script>alert('Mail Sent Successfully');self.close();</script>";
+            this.ClientScript.RegisterClientScriptBlock(this.GetType(), "sendMail", script);
+        }
+
+    }
+}
