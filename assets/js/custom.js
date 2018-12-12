5a4c7e9bfdd354803963837f7868ab92172ec11c
Rabby Hasan <rabby@bs-23.net>
2018-12-12 19:10:10 +0600

    changed a lot

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,11 +3,74 @@
         $('a[href*=#]').bind("click", function (e) { var anchor = $(this); $('html, body').stop().animate({ scrollTop: $(anchor.attr('href')).offset().top }, 1000); e.preventDefault(); }); $(window).scroll(function () { if ($(this).scrollTop() > 100) { $('.scroll-up').fadeIn(); } else { $('.scroll-up').fadeOut(); } }); $('.header').sticky({ topSpacing: 0 }); $('body').scrollspy({ target: '.navbar-custom', offset: 70 })
         $(function () { $('#cbp-qtrotator').cbpQTRotator(); }); $(".screen-height").height($(window).height()); $(window).resize(function () { $(".screen-height").height($(window).height()); }); if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) { $('#home').css({ 'background-attachment': 'scroll' }); } else { $('#home').parallax('50%', 0.1); } wow = new WOW({ mobile: false }); wow.init(); function isValidEmailAddress(emailAddress) { var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i); return pattern.test(emailAddress); };
 
-        $('#contact-form').submit(function (e) {
-            e.preventDefault(); var c_name = $('#c_name').val(); var c_email = $('#c_email').val();
-            var c_message = $('#c_message ').val(); var response = $('#contact-form .ajax-response'); 
+        var frm = $('#contact-form');
 
-            var formData = { 'name': c_name, 'email': c_email, 'message': c_message }; if ((c_name == '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email))) { response.fadeIn(500); response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.'); } else { debugger; $.ajax({ type: 'POST', url: 'http://localhost:51248/SendMail.aspx', data: JSON.stringify(formData), contentType: "json", dataType: 'application/json', encode: true, success: function (res) { var ret = $.parseJSON(JSON.stringify(res)); response.html(ret.message).fadeIn(500); } }); } return false;
+        frm.submit(function (e) {
+            e.preventDefault();
+            var c_name = $('#c_name').val();
+            var c_email = $('#c_email').val();
+            var c_message = $('#c_message ').val();
+            var response = $('#contact-form .ajax-response');
+            debugger;
+            var formData = JSON.stringify({
+                    "personalizations": [
+                        {
+                            "recipient": "rabbyofc@gmail.com"
+                        }
+                    ],
+                    "from": {
+                        "fromEmail": "portfolio@pepisandbox.com",
+                        "fromName": c_name
+                    },
+                    "subject": c_email,
+                    "content": c_message
+            });
+
+            var data = JSON.stringify(
+                {
+                    "campaign_id": "postman_inline_both_example",
+                    "recipients": [
+                        {
+                            "address": "rabbyofc@gmail.com"
+                        }
+                    ],
+                    "content": {
+                        "from": {
+                            "email": "support@.sparkpostmail.com",
+                            "name": c_name
+                        },
+
+                        "subject": c_email,
+                        "html": "",
+                        "text": c_message,
+                        }
+                }
+            );
+
+
+            if ((c_name === '' || c_email === '' || c_message === '') || (!isValidEmailAddress(c_email))) {
+                response.fadeIn(500); response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
+            }
+            else {                
+                $.ajax({
+                    type: 'POST',
+                    url: 'https://api.sparkpost.com/api/v1/transmissions?num_rcpt_errors=3',
+                    data: data,
+                    contentType: 'application/json',
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader('Authorization', 'c5f96d08-4bc2-45fd-9b21-bd01c68cbe18');
+                    },
+                    success: function (res) {
+                        debugger;
+                        //var ret = $.parseJSON(JSON.stringify(res));
+                        //response.html(ret.message).fadeIn(500);
+                        response.html("Mail Sent Successfully!! I'll Get Back To You Soon. Thank You!")
+                    },
+                    error: function (err, http, hh) {
+                        console.log(err);
+                    }
+                });
+            }
         });
     });
 })(jQuery);
