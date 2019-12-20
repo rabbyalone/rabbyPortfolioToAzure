a6a85c8f5229de9fc9c12b93a794678dc4bfed3f
Rabby Hasan <rabby@bs-23.net>
2019-12-20 16:32:50 +0600

    portfolio section updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,7 @@
 		$('#preloader').delay(300).fadeOut('slow');
 	});
 	$(document).ready(function() {
-		$('a[href*="#"]').on('click', function(e) {
+		$('li a[href*="#"]').on('click', function(e) {
 			var anchor = $(this);
 			$('html, body').stop().animate({ scrollTop: $(anchor.attr('href')).offset().top }, 1000);
 			e.preventDefault();
